39f2b9d2b237c52dfaa9760df2cdc38f07a12cda
Canh Toan <nctt96@gmail.com>
2019-04-24 18:00:37 +0700

    Final - AWS

--- models/user.js
+++ models/user.js
@@ -11,13 +11,22 @@ if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production
   	config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 }*/
 
+// For AWS
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+/*// For Google Cloud SQL
 const connection = mysql.createConnection({
 	socketPath  : '/cloudsql/enews-237900:us-east1:enews-mysql',
 	host 		: config.get('mysql.host'),
 	user 		: config.get('mysql.user'),
 	password 	: config.get('mysql.password'),
 	database 	: config.get('mysql.database')
-})
+})*/
 
 connection.connect();
 
